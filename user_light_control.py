777a8974c325534bd3f73a6bc4de6591932eedd0
Emeka Ugwuanyi <emylincon@gmail.com>
2019-11-06 17:17:18 +0000

    added a new file for user to control the led lights using inputs from the user

--- user_light_control.py
+++ user_light_control.py
@@ -4,12 +4,16 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
 
 while True:
-    text = input("Enter off or on: ").lower()
-    if text == 'on':
-        GPIO.output(17, True)
-    elif text == 'off':
-        GPIO.output(17, False)
-    elif text == 'stop':
+    try:
+        text = input("Enter off or on: ").lower()
+        if text == 'on':
+            GPIO.output(17, True)
+        elif text == 'off':
+            GPIO.output(17, False)
+        elif text == 'stop':
+            break
+        else:
+            print("invalid input \n You Typed: ", text)
+    except KeyboardInterrupt:
+        print("\nProgramme Terminated\n")
         break
-    else:
-        print("invalid input \n You Typed: ", text)
