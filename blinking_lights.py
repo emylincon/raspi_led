aab3d9f010cc096e1764f7532d48a43f80042079
Emeka Ugwuanyi <emylincon@gmail.com>
2019-11-08 19:45:38 +0000

    updated file

--- blinking_lights.py
+++ blinking_lights.py
@@ -1,12 +1,64 @@
-import time
 import RPi.GPIO as GPIO
+import os
+from pyfiglet import Figlet
+import sys
+import time
+from termcolor import colored, cprint
 
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.OUT)
+GPIO.setup(17, GPIO.OUT)
 
+
+def light():
+    print('    _------_')
+    print('  -~        ~-')
+    print(' -     _      -')
+    print('-      |>      -')
+    print('-      |<      -')
+    print(' -     |>     -')
+    print('  -    ||    -')
+    print('   -   ||   -')
+    print('    -__||__-')
+    print('    |______|')
+    print('    <______>')
+    print('    <______>')
+    print('       \/')
+
+
+def delay_print(s):
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+
+os.system('clear')
+print('>> starting program...')
+delay_print("<======================================================")
+
+
+def display_text():
+    os.system('clear')
+    custom_fig = Figlet(font='graffiti')
+    cprint(custom_fig.renderText('welcome to LSBU'), 'yellow')
+    cprint("                    /********************************\\", 'yellow')
+    cprint("                    *                                *", 'yellow')
+    cprint("                    *      Use CTR + C to exit       *", 'red')
+    cprint("                    *                                *", 'yellow')
+    cprint("                    \********************************/\n", 'yellow')
+
+
+display_text()
 while True:
-    GPIO.output(4, True)
+    GPIO.output(17, True)
+    light()
+    time.sleep(2)
+    GPIO.output(17, False)
+    os.system('clear')
+    display_text()
     time.sleep(1)
-    GPIO.output(4, False)
-    time.sleep(1)
+
+
+
+
